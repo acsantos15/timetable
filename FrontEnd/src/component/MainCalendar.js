528a2d708542c68e39324a37ed4b606ab95cb426
acsantos <ariescarlo113@gmail.com>
2023-05-09 22:12:23 +0800

    -fetch calendar events -header username

--- FrontEnd/src/component/MainCalendar.js
+++ FrontEnd/src/component/MainCalendar.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 
+import axios from 'axios';
 
-const MainCalendar = () => {
+const MainCalendar = (props) => {
+
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        axios.defaults.withCredentials = true;
+        axios.get('/events')
+            .then(response => {
+                setEvents(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
     const headerToolbar = {
         left: 'title,prev,next,today',
         center: '',
-        right: 'timeGridWeek,dayGridMonth,timeGridDay,list',
+        right: 'timeGridWeek,dayGridMonth,timeGridDay,list customButton',
     };
+
+    const customButton = {
+        text: 'Add Appointment',
+        click: function() {
+            props.handleCustomButtonClick();
+        }
+    };
+
   return (
     <FullCalendar
         plugins={[
@@ -20,6 +43,14 @@ const MainCalendar = () => {
         ]}
         initialView="timeGridWeek"
         headerToolbar={headerToolbar}
+        customButtons={{ customButton }}
+        events={events.map(event => ({
+          title: event.title,
+          start: event.start,
+          end: event.end
+        })
+        
+      )}
     />
   );
 };
