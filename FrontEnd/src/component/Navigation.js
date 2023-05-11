32fdf7833aa5352410f6046f1b853637144b76f9
acsantos <ariescarlo113@gmail.com>
2023-05-11 15:27:30 +0800

    mali si aries

--- FrontEnd/src/component/Navigation.js
+++ FrontEnd/src/component/Navigation.js
@@ -6,6 +6,12 @@ const Navigation = () => {
   return (
     <div className="nav nav-tabs justify-content-center" style={{backgroundColor: '#dedbd3'}}>
 
+          <NavLink className="nav-link" to="/dashboard" activeClassName="active">
+            <span>
+              <i class="fa-sharp fa-regular fa-calendar-days me-2"></i>Dashboard
+            </span>
+          </NavLink>
+
           <NavLink className="nav-link" to="/timetable" activeClassName="active">
             <span>
               <i class="fa-sharp fa-regular fa-calendar-days me-2"></i>Timetable
