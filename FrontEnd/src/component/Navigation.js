92bfaab801b223a5cf6a1f7d7a546faae6946cbe
allyyyyyyyy <aleligayallysa25@gmail.com>
2023-05-11 09:19:38 +0800

    navigation

--- FrontEnd/src/component/Navigation.js
+++ FrontEnd/src/component/Navigation.js
@@ -2,27 +2,22 @@ import React from 'react'
 import "../css/style.css";
 import { NavLink } from 'react-router-dom';
 
-const classNameFunc = ({ isActive }) => (isActive ? "active" : "inactive");
-
 const Navigation = () => {
   return (
-    <div className="nav nav-tabs justify-content-center">
-        {/* <ul className="nav nav-tabs justify-content-center" >
-            <li className="nav-item" >
-                <a style={{color:'#8e8380'}} className="nav-link" href="/dashboard" ><i class="fa-solid fa-grip me-2" ></i>Dashboard</a>
-            </li>
-            <li className="nav-item">
-                <a style={{color:'#a4a1a1'}} className="nav-link" href="/timetable"><i class="fa-sharp fa-regular fa-calendar-days me-2"></i>Timetable</a>
-            </li>
-        </ul> */}
+    <div className="nav nav-tabs justify-content-center" style={{backgroundColor: '#dedbd3'}}>
 
-        <NavLink to="/dashboard" activeClassName={classNameFunc}>
-          <a className="nav-link active" href="/dashboard" ><i class="fa-solid fa-grip me-2" ></i>Dashboard</a>
-        </NavLink>
+          <NavLink className="nav-link" to="/dashboard" activeClassName="active">
+            <span>
+              <i class="fa-sharp fa-regular fa-calendar-days me-2"></i>Dashboard
+            </span>
+          </NavLink>
 
-        <NavLink to="/timetable" activeClassName={classNameFunc}>
-          <a className="nav-link" href="/timetable"><i class="fa-sharp fa-regular fa-calendar-days me-2"></i>Timetable</a>
-        </NavLink>
+          <NavLink className="nav-link" to="/timetable" activeClassName="active">
+            <span>
+              <i class="fa-sharp fa-regular fa-calendar-days me-2"></i>Timetable
+            </span>
+          </NavLink>
+
     </div>
   )
 }
