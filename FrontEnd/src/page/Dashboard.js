4ed99a4f9ad15f325d69f4d90bcac0becc031808
acsantos <ariescarlo113@gmail.com>
2023-05-19 19:41:16 +0800

    -add loading animation until axios responds

--- FrontEnd/src/page/Dashboard.js
+++ FrontEnd/src/page/Dashboard.js
@@ -11,6 +11,8 @@ const Dashboard = () => {
   const [todayEvents, setTodayEvents] = useState([]);
   const [tomorrowEvents, setTomorrowEvents] = useState([]);
   const [weatherData, setWeatherData] = useState({});
+
+  const [Loading, setLoading] = useState(true);
   
     useEffect(() => {
         axios.get('/dashboard')
@@ -18,9 +20,11 @@ const Dashboard = () => {
             setTodayEvents(response.data.today);
             setTomorrowEvents(response.data.tomorrow);
             setWeatherData(response.data.weatherData);
+            setLoading(false)
         })
         .catch((error) => {
             console.log(error);
+            setLoading(false)
         });
     }, []);
 
@@ -118,50 +122,60 @@ const Dashboard = () => {
                     </div>
                     {/* <hr/> */}
                     
-                    
-                    {todayEvents.length ? (
-                    todayEvents.map((event) => (
-                        <div key={event.id} className="dash card" style={{backgroundColor: event.color, margin: '10px', color: 'white', padding: '10px 20px 10px 0'}}>
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-sm-9">
-                                <ul style={{listStyleType: 'none', marginTop: '12px'}}> 
-                                    <li style={{fontSize: 'larger', fontWeight: 'bold'}}>{event.title}</li>
-                                    <li> {event.description}</li>
-                                    <li><small>{moment(event.start).format('hh:mm:ss a')}</small> - <small>{moment(event.end).format('hh:mm:ss a')}</small></li>
-                                    {moment().isBetween(moment(event.start), moment(event.end)) && (
-                                    <li>
-                                        <span class="badge bg-light" style={{ color: 'red', fontWeight: 'bold', fontSize: '15px' }}>ONGOING ​​​​
-                                        <div class="spinner-border text-danger spinner-border-sm" role="status">
-                                            <span class="visually-hidden">Loading...</span>
-                                        </div>
-                                        </span>
-                                    </li>
-                                    )}
-                                </ul>
-                                </div>
-                                <div class="col">
-                                <div class="card" style={{backgroundColor: 'rgb(0 0 0 / 20%)'}}>
-                                    <div class="card-header" style={{fontSize: 'small', fontWeight: 'bold'}}>
-                                        Participant/s
-                                    </div>
-                                    <ul class="list-unstyled pl-1" style={{listStyleType: 'none', margin: '5px'}}>
-                                        {participant[event.id]?.map((participantName) => (
-                                        <li class="list-group-item" style={{fontSize: 'small'}}>•ㅤ{participantName}</li>
-                                        ))}
-                                    </ul>
-                                </div>
-                                </div>
+                    {Loading ? (
+                        // Show the Bootstrap spinner while loading is true
+                        <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100px' }}>
+                            <div className="spinner-border text-success" role="status">
+                                <span className="visually-hidden">Loading...</span>
                             </div>
-                        </div>           
                         </div>
-                        
-                    ))
                     ) : (
-                        <div class="dash card" style={{margin:'10px', color:'white', padding: '20px 20px 20px 0', backgroundColor: 'rgb(68, 66, 66)'}}>
-                            <div class="d-flex align-items-center justify-content-center" style={{fontSize: 'larger', fontWeight: 'bold'}}>No Event for Today</div>
-                        </div>
-                    )}
+                        <>
+                        {todayEvents.length ? (
+                        todayEvents.map((event) => (
+                            <div key={event.id} className="dash card" style={{backgroundColor: event.color, margin: '10px', color: 'white', padding: '10px 20px 10px 0'}}>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-sm-9">
+                                    <ul style={{listStyleType: 'none', marginTop: '12px'}}> 
+                                        <li style={{fontSize: 'larger', fontWeight: 'bold'}}>{event.title}</li>
+                                        <li> {event.description}</li>
+                                        <li><small>{moment(event.start).format('hh:mm:ss a')}</small> - <small>{moment(event.end).format('hh:mm:ss a')}</small></li>
+                                        {moment().isBetween(moment(event.start), moment(event.end)) && (
+                                        <li>
+                                            <span class="badge bg-light" style={{ color: 'red', fontWeight: 'bold', fontSize: '15px' }}>ONGOING ​​​​
+                                            <div class="spinner-border text-danger spinner-border-sm" role="status">
+                                                <span class="visually-hidden">Loading...</span>
+                                            </div>
+                                            </span>
+                                        </li>
+                                        )}
+                                    </ul>
+                                    </div>
+                                    <div class="col">
+                                    <div class="card" style={{backgroundColor: 'rgb(0 0 0 / 20%)'}}>
+                                        <div class="card-header" style={{fontSize: 'small', fontWeight: 'bold'}}>
+                                            Participant/s
+                                        </div>
+                                        <ul class="list-unstyled pl-1" style={{listStyleType: 'none', margin: '5px'}}>
+                                            {participant[event.id]?.map((participantName) => (
+                                            <li class="list-group-item" style={{fontSize: 'small'}}>•ㅤ{participantName}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>           
+                            </div>
+                            
+                        ))
+                        ) : (
+                            <div class="dash card" style={{margin:'10px', color:'white', padding: '20px 20px 20px 0', backgroundColor: 'rgb(68, 66, 66)'}}>
+                                <div class="d-flex align-items-center justify-content-center" style={{fontSize: 'larger', fontWeight: 'bold'}}>No Event for Today</div>
+                            </div>
+                        )}
+                        </>
+                    )}  
                     <div class="card-footer sticky-bottom" style={{backgroundColor: 'white'}}></div>
                 </div>
 
@@ -175,40 +189,60 @@ const Dashboard = () => {
                     </div>
                     {/* <hr/> */}
 
-                    {tomorrowEvents.length ? (
-                    tomorrowEvents.map((event) => (
-                        <div key={event.id} className="dash card" style={{backgroundColor: event.color, margin: '10px', color: 'white', padding: '10px 20px 10px 0'}}>
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-sm-9">
-                                <ul style={{listStyleType: 'none', marginTop: '12px'}}> 
-                                    <li style={{fontSize: 'larger', fontWeight: 'bold'}}>{event.title}</li>
-                                    <li> {event.description}</li>
-                                    <li><small>{moment(event.start).format('hh:mm a')}</small> - <small>{moment(event.end).format('hh:mm a')}</small></li>
-                                </ul>
-                                </div>
-                                <div class="col">
-                                <div class="card" style={{backgroundColor: 'rgb(0 0 0 / 20%)'}}>
-                                    <div class="card-header" style={{fontSize: 'small', fontWeight: 'bold'}}>
-                                        Participant/s
-                                    </div>
-                                    <ul class="list-unstyled pl-1" style={{listStyleType: 'none', margin: '5px'}}>
-                                        {participant[event.id]?.map((participantName) => (
-                                        <li class="list-group-item" style={{fontSize: 'small'}}>•ㅤ{participantName}</li>
-                                        ))}
-                                    </ul>
-                                </div>
-                                </div>
+                    {Loading ? (
+                        // Show the Bootstrap spinner while loading is true
+                        <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100px' }}>
+                            <div className="spinner-border text-success" role="status">
+                                <span className="visually-hidden">Loading...</span>
                             </div>
-                        </div>           
                         </div>
-                        
-                    ))
                     ) : (
-                        <div class="dash card" style={{margin:'10px', color:'white', padding: '20px 20px 20px 0', backgroundColor: 'rgb(68, 66, 66)'}}>
-                            <div class="d-flex align-items-center justify-content-center" style={{fontSize: 'larger', fontWeight: 'bold'}}>No Event for Today</div>
-                        </div>
-                    )}
+                        <>
+                        {tomorrowEvents.length ? (
+                        tomorrowEvents.map((event) => (
+                            <div key={event.id} className="dash card" style={{backgroundColor: event.color, margin: '10px', color: 'white', padding: '10px 20px 10px 0'}}>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-sm-9">
+                                    <ul style={{listStyleType: 'none', marginTop: '12px'}}> 
+                                        <li style={{fontSize: 'larger', fontWeight: 'bold'}}>{event.title}</li>
+                                        <li> {event.description}</li>
+                                        <li><small>{moment(event.start).format('hh:mm:ss a')}</small> - <small>{moment(event.end).format('hh:mm:ss a')}</small></li>
+                                        {moment().isBetween(moment(event.start), moment(event.end)) && (
+                                        <li>
+                                            <span class="badge bg-light" style={{ color: 'red', fontWeight: 'bold', fontSize: '15px' }}>ONGOING ​​​​
+                                            <div class="spinner-border text-danger spinner-border-sm" role="status">
+                                                <span class="visually-hidden">Loading...</span>
+                                            </div>
+                                            </span>
+                                        </li>
+                                        )}
+                                    </ul>
+                                    </div>
+                                    <div class="col">
+                                    <div class="card" style={{backgroundColor: 'rgb(0 0 0 / 20%)'}}>
+                                        <div class="card-header" style={{fontSize: 'small', fontWeight: 'bold'}}>
+                                            Participant/s
+                                        </div>
+                                        <ul class="list-unstyled pl-1" style={{listStyleType: 'none', margin: '5px'}}>
+                                            {participant[event.id]?.map((participantName) => (
+                                            <li class="list-group-item" style={{fontSize: 'small'}}>•ㅤ{participantName}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>           
+                            </div>
+                            
+                        ))
+                        ) : (
+                            <div class="dash card" style={{margin:'10px', color:'white', padding: '20px 20px 20px 0', backgroundColor: 'rgb(68, 66, 66)'}}>
+                                <div class="d-flex align-items-center justify-content-center" style={{fontSize: 'larger', fontWeight: 'bold'}}>No Event for Today</div>
+                            </div>
+                        )}
+                        </>
+                    )}  
                     <div class="card-footer sticky-bottom" style={{backgroundColor: 'white'}}></div>
                 </div>
             </div>
@@ -225,8 +259,19 @@ const Dashboard = () => {
             {/* Weather div */}
             <div className="card align-items-center mb-3" style={{padding: '20px'}}>
               <h3 style={{fontWeight: 'bold', color: '#7993A0'}}>Today's Weather</h3>
-              {/* <img src={`http://openweathermap.org/img/wn/${weatherData?.weather?.[0]?.icon}@2x.png`} alt="weather icon" style={{width: '150px'}} /> */}
-              <img src={weatherIcon} alt="weather icon" style={{width: '150px'}} />
+              <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '200px' }}>
+                {Loading ? (
+                    // Show the Bootstrap spinner while loading is true
+                        <div className="spinner-border text-success" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                ) : (
+                    // Render the content when loading is false
+                    <>
+                    <img src={weatherIcon} alt="weather icon" style={{width: '150px'}} />
+                    </>
+                )}
+              </div>
               <p><b>Location:</b> Pasig</p>
               <p><b>Temperature:</b> {weatherData?.main?.temp}&deg;C</p>
               <p><b>Humidity:</b> {weatherData?.main?.humidity}%</p>
--- FrontEnd/src/page/NotFound.js
+++ FrontEnd/src/page/NotFound.js
@@ -11,7 +11,7 @@ function NotFound() {
         <h1 style={{fontSize: '300px'}}>4<span style={{color: '#537557'}}>0</span>4</h1>
         </div>
         <h2>the page you requested could not found</h2><br></br>
-        <a class="btn btn-outline-primary" style={{borderColor: '#537557', color: '#537557'}} href="/dashboard" role="button">Back to Dashbaord</a>
+        <a class="btn btn-outline-success" style={{borderColor: '#537557', color: '#537557'}} href="/dashboard" role="button">Back to Dashbaord</a>
     </div>
     </div>
     </>
